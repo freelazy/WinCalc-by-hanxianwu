16ad75adc8ace9be5cf7543261d2feb2da00267f
yao7san <yao7san@fake.com>
2022-09-07 15:18:51 +0800

    第一个版本

---
+++ CmputeMachine/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace CmputeMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtAlgorithm = new System.Windows.Forms.TextBox();
+            this.btnChu = new System.Windows.Forms.Button();
+            this.btnResetResult = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btnYu = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDian = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtResult.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtResult.Location = new System.Drawing.Point(4, 103);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(587, 66);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.Text = "0";
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.txtAlgorithm);
+            this.panel1.Controls.Add(this.txtResult);
+            this.panel1.Location = new System.Drawing.Point(13, 22);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 181);
+            this.panel1.TabIndex = 22;
+            // 
+            // txtAlgorithm
+            // 
+            this.txtAlgorithm.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtAlgorithm.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAlgorithm.Location = new System.Drawing.Point(4, 48);
+            this.txtAlgorithm.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAlgorithm.Name = "txtAlgorithm";
+            this.txtAlgorithm.Size = new System.Drawing.Size(587, 39);
+            this.txtAlgorithm.TabIndex = 0;
+            this.txtAlgorithm.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnChu
+            // 
+            this.btnChu.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnChu.Location = new System.Drawing.Point(464, 211);
+            this.btnChu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnChu.Name = "btnChu";
+            this.btnChu.Size = new System.Drawing.Size(143, 80);
+            this.btnChu.TabIndex = 20;
+            this.btnChu.Text = "÷";
+            this.btnChu.UseVisualStyleBackColor = true;
+            this.btnChu.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnResetResult
+            // 
+            this.btnResetResult.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnResetResult.Location = new System.Drawing.Point(13, 211);
+            this.btnResetResult.Margin = new System.Windows.Forms.Padding(4);
+            this.btnResetResult.Name = "btnResetResult";
+            this.btnResetResult.Size = new System.Drawing.Size(143, 80);
+            this.btnResetResult.TabIndex = 19;
+            this.btnResetResult.Text = "CE";
+            this.btnResetResult.UseVisualStyleBackColor = true;
+            this.btnResetResult.Click += new System.EventHandler(this.btnResetResult_Click);
+            // 
+            // btnX
+            // 
+            this.btnX.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnX.Location = new System.Drawing.Point(464, 298);
+            this.btnX.Margin = new System.Windows.Forms.Padding(4);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(143, 80);
+            this.btnX.TabIndex = 18;
+            this.btnX.Text = "×";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnReset.Location = new System.Drawing.Point(163, 211);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(143, 80);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "C";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDel.Location = new System.Drawing.Point(464, 386);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(143, 80);
+            this.btnDel.TabIndex = 16;
+            this.btnDel.Text = "-";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAdd.Location = new System.Drawing.Point(464, 474);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(143, 80);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnResult.Location = new System.Drawing.Point(464, 561);
+            this.btnResult.Margin = new System.Windows.Forms.Padding(4);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(143, 80);
+            this.btnResult.TabIndex = 14;
+            this.btnResult.Text = "=";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn9.Location = new System.Drawing.Point(313, 298);
+            this.btn9.Margin = new System.Windows.Forms.Padding(4);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(143, 80);
+            this.btn9.TabIndex = 13;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn8.Location = new System.Drawing.Point(163, 298);
+            this.btn8.Margin = new System.Windows.Forms.Padding(4);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(143, 80);
+            this.btn8.TabIndex = 21;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn6.Location = new System.Drawing.Point(313, 386);
+            this.btn6.Margin = new System.Windows.Forms.Padding(4);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(143, 80);
+            this.btn6.TabIndex = 2;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn5.Location = new System.Drawing.Point(163, 386);
+            this.btn5.Margin = new System.Windows.Forms.Padding(4);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(143, 80);
+            this.btn5.TabIndex = 10;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnYu
+            // 
+            this.btnYu.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnYu.Location = new System.Drawing.Point(313, 211);
+            this.btnYu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnYu.Name = "btnYu";
+            this.btnYu.Size = new System.Drawing.Size(143, 80);
+            this.btnYu.TabIndex = 9;
+            this.btnYu.Text = "←";
+            this.btnYu.UseVisualStyleBackColor = true;
+            this.btnYu.Click += new System.EventHandler(this.btnYu_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(313, 474);
+            this.btn3.Margin = new System.Windows.Forms.Padding(4);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(143, 80);
+            this.btn3.TabIndex = 8;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn7.Location = new System.Drawing.Point(13, 298);
+            this.btn7.Margin = new System.Windows.Forms.Padding(4);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(143, 80);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(163, 474);
+            this.btn2.Margin = new System.Windows.Forms.Padding(4);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(143, 80);
+            this.btn2.TabIndex = 6;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn4.Location = new System.Drawing.Point(13, 386);
+            this.btn4.Margin = new System.Windows.Forms.Padding(4);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(143, 80);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(13, 474);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(143, 80);
+            this.btn1.TabIndex = 3;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn0.Location = new System.Drawing.Point(163, 561);
+            this.btn0.Margin = new System.Windows.Forms.Padding(4);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(143, 80);
+            this.btn0.TabIndex = 11;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnDian
+            // 
+            this.btnDian.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDian.Location = new System.Drawing.Point(313, 561);
+            this.btnDian.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDian.Name = "btnDian";
+            this.btnDian.Size = new System.Drawing.Size(143, 80);
+            this.btnDian.TabIndex = 4;
+            this.btnDian.Text = ".";
+            this.btnDian.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnDian.UseVisualStyleBackColor = true;
+            this.btnDian.Click += new System.EventHandler(this.btnDian_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 662);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnChu);
+            this.Controls.Add(this.btnResetResult);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btnYu);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btnDian);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TextBox txtResult;
+        private Panel panel1;
+        private TextBox txtAlgorithm;
+        private Button btnChu;
+        private Button btnResetResult;
+        private Button btnX;
+        private Button btnReset;
+        private Button btnDel;
+        private Button btnAdd;
+        private Button btnResult;
+        private Button btn9;
+        private Button btn8;
+        private Button btn6;
+        private Button btn5;
+        private Button btnYu;
+        private Button btn3;
+        private Button btn7;
+        private Button btn2;
+        private Button btn4;
+        private Button btn1;
+        private Button btn0;
+        private Button btnDian;
+    }
+}
---
+++ CmputeMachine/Form1.cs
@@ -0,0 +1,138 @@
+namespace CmputeMachine
+{
+    public partial class Form1 : Form
+    {
+        int biaoji = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// 点击数字事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            //判断上一步是否为 + - * /
+            if (biaoji==1)
+            {
+                txtResult.Text = "0";
+                biaoji = 0;
+            }
+            //判断上一步是否为 =
+            if (biaoji==2)
+            {
+                txtResult.Text = "0";
+                txtAlgorithm.Text = "";
+                biaoji = 0;
+            }
+           Button btn = (Button) sender;
+            //判断是否为0
+            if (txtResult.Text=="0")
+            {
+                txtResult.Text = btn.Text;
+            }
+            else
+            {
+                txtResult.Text += btn.Text;
+            }
+        }
+        private decimal JiSuan()
+        {
+            string fuhao = txtAlgorithm.Text.Substring(txtAlgorithm.Text.Length - 1);
+            decimal jisuan = Convert.ToDecimal(txtAlgorithm.Text.Substring(0, txtAlgorithm.Text.Length - 1));
+            decimal sr = Convert.ToDecimal(txtResult.Text);
+
+            decimal jieguo = 0;
+            switch (fuhao)
+            {
+                case "+": jieguo = jisuan + sr; break;
+                case "-": jieguo = jisuan - sr; break;
+                case "×": jieguo = jisuan * sr; break;
+                case "÷": jieguo = jisuan / sr; break;
+                default:
+                    break;
+            }
+            return jieguo;
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtResult.Text.Substring(txtResult.Text.Length-1)==btnDian.Text)
+            {
+                txtResult.Text= txtResult.Text.Substring(0, txtResult.Text.Length - 1);
+            }
+            Button btn= (Button) sender;
+            //判断计算框是否不为空
+            if (txtAlgorithm.Text!="")
+            {
+                decimal dc = JiSuan();
+                txtResult.Text = dc.ToString();
+
+            }
+            txtAlgorithm.Text = txtResult.Text + btn.Text;
+            
+            //加减乘除标记为1
+            biaoji = 1;
+        }
+
+        /// <summary>
+        /// 点击等于事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            decimal dc= JiSuan();
+            txtAlgorithm.Text = txtAlgorithm.Text + txtResult.Text + btnResult.Text;
+            txtResult.Text = dc.ToString();
+            biaoji = 2;
+        }
+
+        /// <summary>
+        /// 退格事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnYu_Click(object sender, EventArgs e)
+        {
+            //判断输入框长度是否为1
+            if (txtResult.Text.Length==1)
+            {
+                txtResult.Text = "0";
+            }
+            else
+            {
+                txtResult.Text = txtResult.Text.Substring(0, txtResult.Text.Length - 1);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "0";
+            txtAlgorithm.Text = "";
+        }
+
+        private void btnResetResult_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "";
+        }
+
+        /// <summary>
+        /// 点击“.”事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDian_Click(object sender, EventArgs e)
+        {
+            //判断输入项是否不包含“.”
+            if (!txtResult.Text.Contains(btnDian.Text))
+            {
+                txtResult.Text += btnDian.Text;
+            }
+        }
+    }
+}
